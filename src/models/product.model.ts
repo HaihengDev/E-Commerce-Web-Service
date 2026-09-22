5c5fc158c51a5331dd1545c3359baaba5a13678d
HaihengDev <haihenglim9@gmail.com>
2026-09-22 17:13:10 +0700

    added: timestamps and collection name

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,33 +1,36 @@
 import mongoose from 'mongoose';
 import { IProduct } from '../interfaces/product.interface.ts';
 
-const productSchema = new mongoose.Schema<IProduct>({
-  _id: {
-    type: mongoose.Types.ObjectId,
+const productSchema = new mongoose.Schema<IProduct>(
+  {
+    _id: {
+      type: mongoose.Types.ObjectId,
+    },
+    product_name: {
+      type: String,
+      required: true,
+    },
+    product_imgUrl: {
+      type: String,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    discount: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+    },
   },
-  product_name: {
-    type: String,
-    required: true,
-  },
-  product_imgUrl: {
-    type: String,
-  },
-  stock: {
-    type: Number,
-    default: 0,
-  },
-  discount: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  category: {
-    type: mongoose.Types.ObjectId,
-    required: true,
-  },
-});
+  { timestamps: true, collection: 'products' },
+);
 
 export default mongoose.model<IProduct>('Product', productSchema);
