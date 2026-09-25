b2cf1ca30aae611e8cf410ae476a895ca337c67c
HaihengDev <haihenglim9@gmail.com>
2026-09-26 00:07:52 +0700

    added: customer repository and service

--- src/repositories/customer.repository.ts
+++ src/repositories/customer.repository.ts
@@ -2,19 +2,19 @@ import { ICustomer } from '../interfaces/customer.interface.ts';
 import Customer from '../models/customer.model.ts';
 
 class CustomerRepository {
-  async findAll() {
+  async findAll(): Promise<ICustomer[]> {
     return await Customer.find();
   }
 
-  async findById(id: string) {
+  async findById(id: string): Promise<ICustomer | null> {
     return await Customer.findById(id);
   }
 
-  async insert(customer: ICustomer) {
+  async insert(customer: ICustomer): Promise<ICustomer | null> {
     return await Customer.create(customer);
   }
 
-  async delete(id: string) {
+  async delete(id: string): Promise<ICustomer | null> {
     return await Customer.findByIdAndDelete(id);
   }
 }
